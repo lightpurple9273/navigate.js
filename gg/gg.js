f75cba66a4356255eb8ecf6b395df58450bc95e0
lightpurple9273 <187844121+lightpurple9273@users.noreply.github.com>
2025-04-02 16:09:55 +0900

    Update gg.js

--- gg/gg.js
+++ gg/gg.js
@@ -2,10 +2,12 @@ const tools = {
     greet(name) {
         console.log(`Hello, ${name}!`);
     },
+    
     farewell(name) {
         console.log(`Goodbye, ${name}!`);
     },  
-    ClickChallengeButton(){
+    
+    clickChallengeButton(){
         const challengeRadioButton = document.querySelector('[data-qa="game-type-challenge"] input[type="radio"]');
         if (challengeRadioButton) {
             challengeRadioButton.click();
@@ -13,6 +15,14 @@ const tools = {
         } else {
             console.log("Challenge button not found.");
         }
-    }
+    },
+
+    turnDefaultSettingToggleOff() {
+        const checkbox = document.querySelector('.toggle_toggle__qfXpL');
+          if (checkbox && checkbox.checked) {
+                checkbox.checked = false; // Uncheck the checkbox if it's checked
+          }
+    },
+
   
 };
