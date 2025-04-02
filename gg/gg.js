293cac0fbf4d904e97624e72242490f304845405
lightpurple9273 <187844121+lightpurple9273@users.noreply.github.com>
2025-04-02 12:44:40 +0900

    Create gg.js

---
+++ gg/gg.js
@@ -0,0 +1,18 @@
+const gg = {
+    greet(name) {
+        console.log(`Hello, ${name}!`);
+    },
+    farewell(name) {
+        console.log(`Goodbye, ${name}!`);
+    },  
+    ClickChallengeButton{
+        const challengeRadioButton = document.querySelector('[data-qa="game-type-challenge"] input[type="radio"]');
+        if (challengeRadioButton) {
+            challengeRadioButton.click();
+            console.log("Challenge button clicked!");
+        } else {
+            console.log("Challenge button not found.");
+        }
+    }
+  
+};
